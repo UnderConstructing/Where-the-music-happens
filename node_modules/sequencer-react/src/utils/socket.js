f73276fadd23d1b32bc662107819bf60d9573774
Lawrence Wallace <lwallac21@gmail.com>
2021-02-10 21:06:56 -0500

    re-re-removing node_modules

--- node_modules/sequencer-react/src/utils/socket.js
+++
@@ -1,7 +0,0 @@
-import io from "socket.io-client"
-
-const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
-
-const url = isDev ? "http://localhost:3000" : "deployed_url"
-
-export const socket = io(url, { transports: ['websocket']})
