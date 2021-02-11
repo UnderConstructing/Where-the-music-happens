a5c9c93ff3766745c21c5f86b34b4b916a9a2444
Lawrence Wallace <lwallac21@gmail.com>
2021-02-10 20:58:56 -0500

    ghosts

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
