4bb95b00edb6515c1f3c00948c872790ae2445ba
Lawrence Wallace <lwallac21@gmail.com>
2021-02-10 21:16:40 -0500

    SO. MANY. TIMES

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/index.css';
-import App from './components/App';
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
