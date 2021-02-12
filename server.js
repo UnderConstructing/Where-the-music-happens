c4317f3e246b789a06f0c92bb445530d2a7bc3da
Lawrence Wallace <lwallac21@gmail.com>
2021-02-11 22:55:07 -0500

    server deploy

--- server.js
+++ server.js
@@ -19,11 +19,16 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }))
 
-const cors = require('cors');
-// app.use(cors({
-//     origin: "http://localhost:3000",
-//     credentials: true
-// }))
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+      
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
+  
 
 const expressSession = require('express-session')
 app.use(expressSession({
